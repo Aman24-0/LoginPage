2644477708955839a2ef2d71ed153316c76a8ec4
Aman24-0 <dahayataman@gmail.com>
2024-10-27 17:12:23 +0530

    Your descriptive commit message

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 let sign_B = document.querySelector('#sign');
 let login_B = document.querySelector('#login');
+let log_P = document.querySelector('#logPage');
 let sign_P = document.querySelector('#signPage');
 
 
 
 sign_B.addEventListener("click",() => {
-    sign_P.classList.toggle('hidden');
-    sign_P.classList.add('flex');
-    document.body.classList.add('from-[#a8ff78]', 'to-[#78ffd6]');
-    document.body.classList.remove('from-[#00c6ff]', 'to-[#0072ff]');    
+    log_P.classList.remove("fade-in", "show");
+    log_P.classList.add("fade-out", "hide");
+
+    setTimeout( ()=>{
+        sign_P.classList.toggle('hidden');
+        sign_P.classList.add('flex');
+    }, 800)
+
+    setTimeout( ()=> {
+
+        sign_P.classList.add("fade-in", "show");
+        sign_P.classList.remove("fade-out", "hide");
+
+          document.body.classList.add('from-[#a8ff78]', 'to-[#78ffd6]');
+          document.body.classList.remove('from-[#00c6ff]', 'to-[#0072ff]');
+    }, 1000);
 })
 
 login_B.addEventListener("click",() => {
-    sign_P.classList.toggle('hidden');
-    document.body.classList.remove('from-[#a8ff78]', 'to-[#78ffd6]');
-    document.body.classList.add('from-[#00c6ff]', 'to-[#0072ff]'); 
+    sign_P.classList.remove("fade-in", "show");
+    sign_P.classList.add("fade-out", "hide");
+
+    setTimeout( ()=> {  
+        sign_P.classList.toggle('hidden');
+        sign_P.classList.remove('flex');
+
+        log_P.classList.add("fade-in", "show");
+        log_P.classList.remove("fade-out", "hide");
+          
+        document.body.classList.remove('from-[#a8ff78]', 'to-[#78ffd6]');
+        document.body.classList.add('from-[#00c6ff]', 'to-[#0072ff]');
+    }, 1000);
 })
 
 
